2ee8eb672c9c8d295d625592ecd95aec913aeae8
rodionbgd <rodionbgd@mail.ru>
2022-02-05 13:37:59 +0300

    Install app

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ function installApp() {
     if ("serviceWorker" in navigator) {
         window.addEventListener("load", () => {
             navigator.serviceWorker
-                .register("/weather_test/sw.js")
+                .register("https://rodionbgd.github.io/weather_test/sw.js")
                 .then((registration) => {
                     console.log("SW registered: ", registration);
                 })
