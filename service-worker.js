1958371af48d2b76a16fa82a765e262030a03d7d
rodionbgd <rodionbgd@mail.ru>
2022-02-05 10:39:10 +0000

    Deploying to gh-pages from  @ 2ee8eb672c9c8d295d625592ecd95aec913aeae8 🚀

--- service-worker.js
+++ service-worker.js
@@ -298,7 +298,7 @@ define(['./workbox-37481be9'], (function (workbox) { 'use strict';
     "revision": "51201249896570bb2aec2b4fc0290053"
   }, {
     "url": "main.bundle.js",
-    "revision": "8052223fcc5e38547ea5acedae67011f"
+    "revision": "54ab1de2b363a8477f1383059033d526"
   }, {
     "url": "main.css",
     "revision": "aef2ebd6a7d44a2087bddfd89f9cedd1"
