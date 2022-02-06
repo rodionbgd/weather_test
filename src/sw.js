f356f097b9204ad34b3aca56669267893190e6fa
rodionbgd <rodionbgd@mail.ru>
2022-02-06 17:33:11 +0300

    Install app

--- src/sw.js
+++ src/sw.js
@@ -18,11 +18,8 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("[ServiceWorker] Activate");
   event.waitUntil(
     (async () => {
-      // Enable navigation preload if it's supported.
-      // See https://developers.google.com/web/updates/2017/02/navigation-preload
       if ("navigationPreload" in self.registration) {
         await self.registration.navigationPreload.enable();
       }
