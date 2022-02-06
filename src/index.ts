1310f05af2156f4216de87f27270d99f1c5b61ce
rodionbgd <rodionbgd@mail.ru>
2022-02-06 21:53:31 +0300

    Stable version

--- src/index.ts
+++ src/index.ts
@@ -12,85 +12,91 @@ window.TOUCH = true;
 // window.TOUCH = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0);
 
 function installApp() {
-    const installAppEl = <HTMLButtonElement>document.getElementById("install-app");
-    const isStandalone = window.matchMedia("(display-mode: standalone)").matches;
+  const installAppEl = <HTMLButtonElement>document.getElementById("install-app");
+  const isStandalone = window.matchMedia("(display-mode: standalone)").matches;
 
-    let promptEvent: BeforeInstallPromptEvent;
+  let promptEvent: BeforeInstallPromptEvent;
 
-    // present install prompt to user
-    async function presentAddToHome() {
-        await promptEvent.prompt();  // Wait for the user to respond to the prompt
-        await promptEvent.userChoice;
-        promptEvent  = <BeforeInstallPromptEvent><unknown>null;
-    }
+  // present install prompt to user
+  async function presentAddToHome() {
+    promptEvent.prompt();
+    promptEvent.userChoice
+      .then(choiceResult => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("user accepted A2HS prompt");
+        } else {
+          console.log("user dismissed A2HS prompt");
+        }
+        promptEvent =<BeforeInstallPromptEvent><unknown> null;
+      });
+  }
 
-    function listenToUserAction() {
-        installAppEl.addEventListener("click", presentAddToHome);
-    }
+  function listenToUserAction() {
+    installAppEl.addEventListener("click", presentAddToHome);
+  }
 
-    // Capture event and defer
-    window.addEventListener("beforeinstallprompt", (e: Event) => {
-        e.preventDefault();
-        promptEvent = <BeforeInstallPromptEvent>e;
-        listenToUserAction();
-    });
+  // Capture event and defer
+  window.addEventListener("beforeinstallprompt", (e: Event) => {
+    e.preventDefault();
+    promptEvent = <BeforeInstallPromptEvent>e;
+    listenToUserAction();
+  });
 
-    if (!isStandalone) {
-        window.dispatchEvent(new Event("beforeinstallprompt"));
-    }
-
-
-
-    // window.addEventListener("beforeinstallprompt", (event) => {
-    //   // Запрет показа информационной мини-панели на мобильных устройствах.
-    //   // alert("beforeinstallprompt")
-    //   if (isStandalone) {
-    //     event.preventDefault();
-    //   }
-    //   // Убираем событие, чтобы его можно было активировать позже.
-    //   window.deferredPrompt = <BeforeInstallPromptEvent>event;
-    //   installAppEl.style.display = "block";
-    // });
+  if (!isStandalone) {
+    window.dispatchEvent(new Event("beforeinstallprompt"));
+  }
 
 
-    // Installation must be done by a user gesture! Here, the button click
-    // installAppEl.addEventListener("click", async () => {
-    //   installAppEl.style.display = "none";
-    //   const promptEvent = window.deferredPrompt;
-    //   if (!promptEvent) {
-    //     return;
-    //   }
-    //   // Показать запрос на установку.
-    //   await promptEvent.prompt();
-    //   // Записать результат в журнал.
-    //   await promptEvent.userChoice;
-    //   // prompt() можно вызвать только один раз.
-    //   window.deferredPrompt = <BeforeInstallPromptEvent><unknown>null;
-    //   // Скрыть кнопку установки.
-    // });
+  // window.addEventListener("beforeinstallprompt", (event) => {
+  //   // Запрет показа информационной мини-панели на мобильных устройствах.
+  //   // alert("beforeinstallprompt")
+  //   if (isStandalone) {
+  //     event.preventDefault();
+  //   }
+  //   // Убираем событие, чтобы его можно было активировать позже.
+  //   window.deferredPrompt = <BeforeInstallPromptEvent>event;
+  //   installAppEl.style.display = "block";
+  // });
 
-    window.addEventListener("appinstalled", (event) => {
-        window.deferredPrompt = <BeforeInstallPromptEvent><unknown>null;
-    });
 
-    if ("serviceWorker" in navigator) {
-        window.addEventListener("load", () => {
-            navigator.serviceWorker
-              .register("https://rodionbgd.github.io/weather_test/sw.js")
-              .then((registration) => {
-                  console.log("SW registered: ", registration);
-              })
-              .catch((registrationError) => {
-                  console.log("SW registration failed: ", registrationError);
-              });
+  // Installation must be done by a user gesture! Here, the button click
+  // installAppEl.addEventListener("click", async () => {
+  //   installAppEl.style.display = "none";
+  //   const promptEvent = window.deferredPrompt;
+  //   if (!promptEvent) {
+  //     return;
+  //   }
+  //   // Показать запрос на установку.
+  //   await promptEvent.prompt();
+  //   // Записать результат в журнал.
+  //   await promptEvent.userChoice;
+  //   // prompt() можно вызвать только один раз.
+  //   window.deferredPrompt = <BeforeInstallPromptEvent><unknown>null;
+  //   // Скрыть кнопку установки.
+  // });
+
+  window.addEventListener("appinstalled", (event) => {
+    window.deferredPrompt = <BeforeInstallPromptEvent><unknown>null;
+  });
+
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("https://rodionbgd.github.io/weather_test/sw.js")
+        .then((registration) => {
+          console.log("SW registered: ", registration);
+        })
+        .catch((registrationError) => {
+          console.log("SW registration failed: ", registrationError);
         });
-    }
+    });
+  }
 }
 
 export const store = configureStore({
-    reducer: {
-        cities: citySlice.reducer
-    }
+  reducer: {
+    cities: citySlice.reducer
+  }
 });
 
 export let mainSwiper: Swiper;
@@ -104,63 +110,63 @@ export let showCity: HTMLDivElement;
 export let updateLocation: HTMLElement;
 
 export function createSwiper() {
-    // const originLocation = window.location.origin;
-    mainSwiper = new Swiper(".swiper", {
-        pagination: {
-            el: ".swiper-pagination",
-            dynamicBullets: true,
-            dynamicMainBullets: 3
-        },
-        watchOverflow: true
-        // history: {
-        //     key: "city",
-        //     root: originLocation,
-        // },
-    });
-    mainSwiper.on("slideChange", () => {
-        const { cities } = store.getState();
-        const currentSlide = <HTMLElement>cityListEl.children[mainSwiper.realIndex];
-        const currentCity = cities.filter(
-          (city) => city.name === currentSlide.dataset.name
-        )[0];
-        const imgList = Array.from(bgContainer.children);
-        const isSameWeather = imgList.filter(
-          (el) =>
-            el.className === `bg-${currentCity.weather.current.weather[0].icon}`
-        ).length;
-        if (isSameWeather) {
-            return;
+  // const originLocation = window.location.origin;
+  mainSwiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      dynamicMainBullets: 3
+    },
+    watchOverflow: true
+    // history: {
+    //     key: "city",
+    //     root: originLocation,
+    // },
+  });
+  mainSwiper.on("slideChange", () => {
+    const { cities } = store.getState();
+    const currentSlide = <HTMLElement>cityListEl.children[mainSwiper.realIndex];
+    const currentCity = cities.filter(
+      (city) => city.name === currentSlide.dataset.name
+    )[0];
+    const imgList = Array.from(bgContainer.children);
+    const isSameWeather = imgList.filter(
+      (el) =>
+        el.className === `bg-${currentCity.weather.current.weather[0].icon}`
+    ).length;
+    if (isSameWeather) {
+      return;
+    }
+    Array.from(bgContainer.children as unknown as HTMLDivElement[]).forEach(
+      (img) => {
+        img.classList.toggle("hidden");
+        img.classList.remove("default-bg");
+        if (img.classList.contains("hidden")) {
+          img.style.setProperty("opacity", "0");
+        } else {
+          img.style.setProperty("opacity", "1");
+          img.setAttribute(
+            "class",
+            `bg-${currentCity.weather.current.weather[0].icon}`
+          );
         }
-        Array.from(bgContainer.children as unknown as HTMLDivElement[]).forEach(
-          (img) => {
-              img.classList.toggle("hidden");
-              img.classList.remove("default-bg");
-              if (img.classList.contains("hidden")) {
-                  img.style.setProperty("opacity", "0");
-              } else {
-                  img.style.setProperty("opacity", "1");
-                  img.setAttribute(
-                    "class",
-                    `bg-${currentCity.weather.current.weather[0].icon}`
-                  );
-              }
-          }
-        );
-    });
+      }
+    );
+  });
 }
 
 export function initializeElements() {
-    app = <HTMLDivElement>document.getElementById("app");
-    bgContainer = <HTMLDivElement>document.getElementById("bg-container");
-    menuEl = <HTMLDivElement>document.getElementById("menu");
-    menuCityList = <HTMLUListElement>document.getElementById("menu-city-list");
-    cityListEl = <HTMLDivElement>document.getElementById("city-list");
-    showCity = <HTMLDivElement>document.getElementById("show-last-city");
-    updateLocation = <HTMLElement>document.getElementById("update-location");
-    installApp();
-    addGoogleScript();
-    listeners();
-    if (window.TOUCH) {
-        createSwiper();
-    }
+  app = <HTMLDivElement>document.getElementById("app");
+  bgContainer = <HTMLDivElement>document.getElementById("bg-container");
+  menuEl = <HTMLDivElement>document.getElementById("menu");
+  menuCityList = <HTMLUListElement>document.getElementById("menu-city-list");
+  cityListEl = <HTMLDivElement>document.getElementById("city-list");
+  showCity = <HTMLDivElement>document.getElementById("show-last-city");
+  updateLocation = <HTMLElement>document.getElementById("update-location");
+  installApp();
+  addGoogleScript();
+  listeners();
+  if (window.TOUCH) {
+    createSwiper();
+  }
 }
