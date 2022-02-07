e355dad1e3f84d4c581fd948d6720b873e33e13c
rodionbgd <rodionbgd@mail.ru>
2022-02-07 20:55:30 +0300

    Определение display mode

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ Swiper.use([Pagination, History]);
 
 // window.TOUCH = true;
 window.TOUCH = "ontouchstart" in window || navigator.maxTouchPoints > 0;
-window.standalone = window.matchMedia("(display-mode: standalone)").matches;
+window.standalone = (navigator as any).standalone || (window.screen.height-document.documentElement.clientHeight<40);
 
 export const store = configureStore({
   reducer: {
